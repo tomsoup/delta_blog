22b2719fc142c8cdfcd9d873118f1a6affdba209
Tom Ka Chun Cheung <tomhk1123@gmail.com>
2017-03-15 02:10:23 -0400

    update and create

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,47 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+
   def index
     @portfolios = Portfolio.all
   end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: 'Blog was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Blog was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints
+    def set_portfolio
+      @portfolio = Portfolio.find(params[:id])
+    end
+
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body)
+    end
+
 end
