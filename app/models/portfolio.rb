41a72a3ec81f0e3e55434ca4cf29d307cbe1aee3
Tom Ka Chun Cheung <tomhk1123@gmail.com>
2017-06-08 15:59:06 -0400

    add default image, scopes and updated seed

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,17 @@
 class Portfolio < ApplicationRecord
-  validates_presence_of :title, :body, :main_iamge, :thumb_image
+  validates_presence_of :title, :body, :main_image, :thumb_image
+
+  # def self.angular
+  #   where(subtitle: 'React')
+  # end
+
+  #scope lamda name are arbitary and can be called in controller
+  scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||=  "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,18 @@
+3.times do |topic|
+  Topic.create!(
+  title: "Topic #{topic}"
+  )
+end
+
 10.times do |blog|
   Blog.create!(
   title: "My Blog Post #{blog}",
   body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac tempus magna. Aliquam sed suscipit arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut hendrerit tellus. Nunc sit amet sem non leo rutrum convallis. Morbi in faucibus justo. Morbi quis tellus id ex pharetra ultrices. Curabitur eu enim sapien. Nunc molestie leo sem, ut elementum magna gravida ut. Etiam quis tortor in nulla laoreet condimentum scelerisque at mi. Fusce eu nulla tempor, fermentum ligula in, scelerisque tellus. Phasellus rutrum augue mi, eget faucibus nisl consectetur vel. Aenean pharetra dolor id ante pretium, ut pellentesque orci tincidunt.
 
-  Maecenas accumsan risus varius ex sagittis maximus. Phasellus neque sapien, venenatis a scelerisque gravida, gravida eu dui. Nulla nisl eros, posuere a vehicula eu, luctus vel augue. Nullam efficitur rutrum tincidunt. Proin faucibus quam diam, vel auctor quam feugiat quis. Donec bibendum est mollis nunc blandit, vel egestas erat maximus. Nam eget posuere ipsum. Mauris lacinia arcu non nisi varius, vel dignissim purus semper. In hac habitasse platea dictumst. Praesent consectetur ut ante eu faucibus."
+  Maecenas accumsan risus varius ex sagittis maximus. Phasellus neque sapien, venenatis a scelerisque gravida, gravida eu dui. Nulla nisl eros, posuere a vehicula eu, luctus vel augue. Nullam efficitur rutrum tincidunt. Proin faucibus quam diam, vel auctor quam feugiat quis. Donec bibendum est mollis nunc blandit, vel egestas erat maximus. Nam eget posuere ipsum. Mauris lacinia arcu non nisi varius, vel dignissim purus semper. In hac habitasse platea dictumst. Praesent consectetur ut ante eu faucibus.",
+  topic_id: Topic.last.id
   )
+
 end
 
 puts "10 blog posts created"
@@ -18,10 +26,20 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+4.times do |portfolio_item|
   Portfolio.create!(
   title: "Portfolio title: #{portfolio_item}",
-  subtitle: "What I did",
+  subtitle: "Ruby",
+  body: "Maecenas accumsan risus varius ex sagittis maximus. Phasellus neque sapien, venenatis a scelerisque gravida, gravida eu dui. Nulla nisl eros, posuere a vehicula eu, luctus vel augue. Nullam efficitur rutrum tincidunt. Proin faucibus quam diam, vel auctor quam feugiat quis. Donec bibendum est mollis nunc blandit, vel egestas erat maximus. Nam eget posuere ipsum. Mauris lacinia arcu non nisi varius, vel dignissim purus semper. In hac habitasse platea dictumst. Praesent consectetur ut ante eu faucibus.",
+  main_image: "http://placehold.it/600x400",
+  thumb_image: "http://placehold.it/350x200"
+  )
+end
+
+3.times do |portfolio_item|
+  Portfolio.create!(
+  title: "Portfolio title: #{portfolio_item}",
+  subtitle: "React",
   body: "Maecenas accumsan risus varius ex sagittis maximus. Phasellus neque sapien, venenatis a scelerisque gravida, gravida eu dui. Nulla nisl eros, posuere a vehicula eu, luctus vel augue. Nullam efficitur rutrum tincidunt. Proin faucibus quam diam, vel auctor quam feugiat quis. Donec bibendum est mollis nunc blandit, vel egestas erat maximus. Nam eget posuere ipsum. Mauris lacinia arcu non nisi varius, vel dignissim purus semper. In hac habitasse platea dictumst. Praesent consectetur ut ante eu faucibus.",
   main_image: "http://placehold.it/600x400",
   thumb_image: "http://placehold.it/350x200"
