77a00b894c376f47db84d3e04c57014789d0be1c
Tom Ka Chun Cheung <tomhk1123@gmail.com>
2017-06-08 14:49:32 -0400

    adds validation

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
   root 'pages#home'
-
 end
